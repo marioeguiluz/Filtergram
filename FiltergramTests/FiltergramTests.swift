906063d3af00502305ddbb94680031bbfa3710f2
marioeguiluz <mario@myplacesandme.com>
2016-12-15 08:28:41 +0000

    Initial Commit

---
+++ FiltergramTests/FiltergramTests.swift
@@ -0,0 +1,36 @@
+//
+//  FiltergramTests.swift
+//  FiltergramTests
+//
+//  Created by MARIO EGUILUZ ALEBICTO on 15/12/2016.
+//  Copyright © 2016 MARIO EGUILUZ ALEBICTO. All rights reserved.
+//
+
+import XCTest
+@testable import Filtergram
+
+class FiltergramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
